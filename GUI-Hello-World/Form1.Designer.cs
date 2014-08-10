7ba924e72295e77145e6d16c27bd6827f63eb1df
Ayamin0x539 <ayamin1337@yahoo.com>
2014-08-10 12:55:55 -0400

    GUI-based Multiplication Game, first commit

---
+++ GUI-Hello-World/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace GUI_Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Fuchsia;
+            this.checkBox1.Location = new System.Drawing.Point(537, 404);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Ready to answer?";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(537, 292);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 106);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(546, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "What is 1 x 1?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.progressBar1.Location = new System.Drawing.Point(12, 43);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(114, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Level: 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(649, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ayamin\'s Multiplication Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GUI-Hello-World/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Hello_World
+{
+    public partial class Form1 : Form
+    {
+        private String szInput = "null"; // for storing input
+        private int level = 1;
+        // All games will start out with "What is 1 x 1?"
+        private int multiplicand = 1;
+        private int multiplier = 1;
+        private int product = 1;
+        private int expOverflow = 0;
+        // We will randomly generate the multiplicand and multiplier.
+        private Random rng = new Random();
+        // Flag keeping track of whether the player is correct or not.
+        private bool fCorrect;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked)
+            {
+                // Compute the product, convert to string to compare with user's input in the TextBox.
+                product = multiplicand * multiplier;
+                fCorrect = (product.ToString() == szInput);
+                // Case: User has not entered anything in the box
+                if (szInput == "null" || szInput == "")
+                {
+                    MessageBox.Show("Please type your answer in the text box.");
+                }
+                else if (fCorrect)
+                {
+                    // Experience gained will scale with the difficulty of the question: exp gained will be 10 times the product.
+                    // Check to see if we have excess experience that goes past the maximum:
+                    if (this.progressBar1.Value + product * 10 >= this.progressBar1.Maximum)
+                    {
+                        // If so, calculate the left over experience:
+                        expOverflow = product * 10 - (this.progressBar1.Maximum - this.progressBar1.Value);
+                    }
+                    this.progressBar1.Increment(product*10); // peform the actual increment
+                    MessageBox.Show("Correct! " + multiplicand + " x " + multiplier + " = " + product + "!\nYou gained some experience.");
+                    if (this.progressBar1.Value == this.progressBar1.Maximum) // If the experience bar hits the required amount...
+                    {
+                        this.label2.Text = "Level " + ++level; // ... we'll level up the player...
+                        this.progressBar1.Value = 0; // ... reset the experience bar ...
+                        this.progressBar1.Maximum = level * level * 50; // ... increase the experience bar to f(x) = 50x^2 so it gets harder to level as level increases ...
+                        this.progressBar1.Increment(expOverflow); // ... add in the excess experience from the previous level ...
+                        expOverflow = 0; // reset the excess counter, so we don't accidentally add it in unless there is actually an overflow
+                        MessageBox.Show("You levelled up! You are now level " + level); // ... and send a message to the player notifying of the level up.
+                    }
+                    // Randomly generate the new question and update the label: (we will increase the number range once level 7 is reached)
+                    if (level < 7)
+                    {
+                        multiplicand = rng.Next(1, 13);
+                        multiplier = rng.Next(1, 13);
+                    }
+                    else
+                    {
+                        multiplicand = rng.Next(1, 20);
+                        multiplier = rng.Next(1, 20);
+                    }
+                    this.label1.Text = "What is " + multiplicand + " x " + multiplier + "?";
+                }
+                else
+                {
+                    MessageBox.Show("Sorry, " + multiplicand + " x " + multiplier + " is not " + szInput + ".\nTry again!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Looks like you left the box unticked! \nYour answer will not be checked unless the box is ticked.");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Stores the textbox string into szInput whenever th textbox is updated:
+            this.szInput = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Experience: " + this.progressBar1.Value + " out of " + this.progressBar1.Maximum);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
